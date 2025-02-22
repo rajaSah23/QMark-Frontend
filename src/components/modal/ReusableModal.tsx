97b63117701fd459c7189c9ff1e847dbc0972bc4
rajaSah23 <rajasah143rk@gmail.com>
2025-02-22 13:28:52 +0530

    fixed responsiveness of create Q form

--- src/components/modal/ReusableModal.tsx
+++ src/components/modal/ReusableModal.tsx
@@ -1,10 +1,12 @@
-import { useDisclosure } from '@mantine/hooks';
+import { useDisclosure, useMediaQuery } from '@mantine/hooks';
 import { Modal, Button } from '@mantine/core';
 import AddQuestion from '../../pages/question/AddQuestion';
 
 
 const ReusableModal = ({title}:any) => {
     const [opened, { open, close }] = useDisclosure(false);
+    const isMobile = useMediaQuery('(max-width: 768px)'); // Check if the screen width is less than 768px
+
 
     return (
         <>
@@ -16,7 +18,7 @@ const ReusableModal = ({title}:any) => {
                     backgroundOpacity: 0.55,
                     blur: 3,
                 }}
-                size={"70%"}
+                size={isMobile ? "100%" : "70%"} 
                 className='border border-red-500'
             >
                 {/* Modal content */}
